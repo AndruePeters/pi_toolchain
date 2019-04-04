6558e01aca0e21eaf98ba989f74d26ac12da653c
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-04-04 12:46:04 -0400

    Removed subdirectory status and added libraries as normal folders

---
+++ libs/wiringPi/DRUE_README.md
@@ -0,0 +1,1 @@
+If you ever need to recompile, the proper CMake files are in wiringPi/
---
+++ libs/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/drue/Documents/pi/wiringPi/wiringPi/ads1115.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/ads1115.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/bmp180.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/bmp180.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/drcNet.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/drcNet.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/drcSerial.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/drcSerial.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/ds18b20.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/ds18b20.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/htu21d.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/htu21d.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/max31855.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/max31855.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/max5322.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/max5322.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/mcp23008.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/mcp23008.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/mcp23016.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/mcp23016.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/mcp23017.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/mcp23017.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/mcp23s08.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/mcp23s08.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/mcp23s17.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/mcp23s17.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/mcp3002.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/mcp3002.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/mcp3004.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/mcp3004.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/mcp3422.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/mcp3422.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/mcp4802.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/mcp4802.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/pcf8574.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/pcf8574.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/pcf8591.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/pcf8591.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/piHiPri.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/piHiPri.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/piThread.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/piThread.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/pseudoPins.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/pseudoPins.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/rht03.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/rht03.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/sn3218.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/sn3218.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/softPwm.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/softPwm.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/softServo.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/softServo.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/softTone.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/softTone.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/sr595.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/sr595.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/wiringPi.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/wiringPi.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/wiringPiI2C.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/wiringPiI2C.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/wiringPiSPI.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/wiringPiSPI.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/wiringSerial.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/wiringSerial.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/wiringShift.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/wiringShift.c.o"
+  "/home/drue/Documents/pi/wiringPi/wiringPi/wpiExtensions.c" "/home/drue/Documents/pi/wiringPi/wiringPi/CMakeFiles/wiringPi.dir/wpiExtensions.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
